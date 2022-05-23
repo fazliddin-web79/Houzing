3c159d43233480dd6080ba13788f3e98049d7fc7
Fazliddin-web-79 <YakubjonovFazliddin777@gmail.com>
2022-05-23 12:47:57 +0500

    bu Search  qismining tayyori

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import "./index.css";
 import Client from "./components/client";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
+import SearchPanel from "./components/searchPanel";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Navbar />
+    <SearchPanel />
     <Client />
     <Footer />
   </React.StrictMode>
