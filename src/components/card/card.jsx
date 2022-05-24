2277e0cd6fceecfafa50e1dbf8a0c29e1133d1d4
Fazliddin-web-79 <YakubjonovFazliddin777@gmail.com>
2022-05-24 12:10:26 +0500

    this is liked end count section done

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -4,12 +4,38 @@ import bath from "../icons/bath.svg";
 import garage from "../icons/garage.svg";
 import ruller from "../icons/ruller.svg";
 import like from "../icons/like.svg";
-import vektor from "../icons/vektor.svg";
+import liked from "../icons/lined.svg";
+import upPrice from "../icons/up-price.svg";
+import downPrice from "../icons/down-price.svg";
 
 class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLike: like,
+      wasLiked: false,
+      count: 0,
+    };
+  }
   render() {
     const { title, info, price } = this.props.data;
-    console.log(this.props.data);
+    const isLiked = () => {
+      if (!this.state.wasLiked) {
+        this.setState({ isLike: liked, wasLiked: true });
+      } else {
+        this.setState({ isLike: like, wasLiked: false });
+      }
+    };
+    const onUp = () => {
+      if (this.state.count < 10) {
+        this.setState({ count: this.state.count + 1 });
+      }
+    };
+    const onDown = () => {
+      if (this.state.count > 0) {
+        this.setState({ count: this.state.count - 1 });
+      }
+    };
     return (
       <div className="card">
         <div
@@ -55,9 +81,20 @@ class Card extends Component {
               <h3>${price.before}/mo</h3>
               <h3>${price.after}/mo</h3>
             </div>
+            <div className="buys-card">
+              <h2>{this.state.count}</h2>
+            </div>
             <div className="card-control-btn">
-              <img src={vektor} alt="this is vektor" />
-              <img src={like} alt="this is vektor" />
+              <div className="is-buy">
+                <img src={upPrice} onClick={onUp} alt="this is vektor" />
+                <img src={downPrice} onClick={onDown} alt="this is vektor" />
+              </div>
+              <img
+                src={this.state.isLike}
+                onClick={isLiked}
+                className="is-like"
+                alt="this is vektor"
+              />
             </div>
           </div>
         </div>
